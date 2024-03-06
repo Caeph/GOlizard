3f9680b6b336ff6dd9cd06fb7c3a5df8fd0bb184
Caeph <katerina.faltejskova@seznam.cz>
2024-03-06 12:46:50 +0100

    initial

---
+++ src/GOlizard/functionality.py
@@ -0,0 +1,14 @@
+
+def predict_GO(sequence):
+    validated_seq = ...
+    return __predict(validated_seq)
+
+
+def predict_GO_fasta(fasta_file):
+    # read fasta
+    # validate
+    validated_seq = ...
+    return __predict(validated_seq)
+
+def __predict(seq):
+    pass
